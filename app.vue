90d31232ec15f796dbccb34f2ec2eea1277303fd
Kuan Kiat <adrianteh02@hotmail.com>
2023-11-25 17:36:16 +0800

    Utils : Add DebugCurrentUser Top bar

--- app.vue
+++ app.vue
@@ -1,43 +1,18 @@
 <template lang="html">
-  <!-- <div
-    class="position-fixed z-1 text-break text-center text-dark bg-danger w-100"
-    style="--bs-bg-opacity: 0.5"
-    v-if="currentUser"
-  >
-    &lt;DEBUG&gt; Current User :
-    <b>{{ currentUser.providerData }} </b>
-  </div> -->
+  <!-- <DebugCurrentUserBar /> -->
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
 
 <script>
-// import { useAuthStore } from './stores/authStore'
-
 export default {
   data() {
-    // const currentUser = ref(null)
-    // const authStore = useAuthStore()
-    // return { currentUser, authStore }
+    return {}
   },
-  created() {
-    // useFirebaseAuth.onAuthStateChanged(useFirebaseAuth.auth, (user) => {
-    //   this.currentUser = user
-    //   if (!!this.currentUser) {
-    //     this.login(this.currentUser)
-    //   }
-    // })
-  },
+  created() {},
   mounted() {},
-  methods: {
-    // login() {
-    //   this.authStore.setUser(this.currentUser)
-    // },
-    // logout() {
-    //   this.authStore.setUser(null)
-    // }
-  }
+  methods: {}
 }
 </script>
 
---
+++ components/DebugCurrentUserBar.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div
+    class="position-fixed z-1 text-break text-center text-dark bg-danger w-100 pe-none"
+    style="--bs-bg-opacity: 0.5"
+    v-if="currentUser"
+  >
+    &lt;DEBUG&gt; Current User :
+    <b>{{ currentUser.providerData }} </b>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    const currentUser = ref(null)
+    return { currentUser }
+  },
+  created() {
+    useFirebaseAuth.onAuthStateChanged(useFirebaseAuth.auth, (user) => {
+      this.currentUser = user
+      if (!!this.currentUser) {
+        this.login(this.currentUser)
+      }
+    })
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
